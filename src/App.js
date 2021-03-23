84a90d65d2c6f443d486fca8735ec1cd2e198caa
AlaaAlsalem <alsalem.aladdin@gmail.com>
2021-03-23 11:35:43 +0200

    Components Part 1

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import {Navbar, NavbarBrand} from 'reactstrap';
+import Menu from './components/MenuComponent';
+
 
 function App() {
   return (
@@ -10,6 +12,7 @@ function App() {
             <NavbarBrand href="/"> Ristorante Con Fusion</NavbarBrand>
           </div>
         </Navbar>
+        <Menu/>
     </div>
   );
 }
